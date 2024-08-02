c0e0905b86da83560acd9a706c5125422e4b1896
Marharyta23 <144152456+Marharyta23@users.noreply.github.com>
2024-08-02 09:53:56 +0000

    Deploying to gh-pages from @ Marharyta23/test-tspace@197f6073baebd567c05dd8927e69d10eb6d17c7f 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import"./assets/style-94ed0582.js";const o=JSON.parse(sessionStorage.getItem("dataObj"));console.log(o);
+import"./assets/style-2703f0ef.js";const o=JSON.parse(sessionStorage.getItem("dataObj"));console.log(o);
 //# sourceMappingURL=commonHelpers2.js.map
